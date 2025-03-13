ff0450dfb06ad05f24abc1da84708c9b513a86a8
Martin Mauch <martin@mauch.dev>
2025-03-13 15:47:51 +0100

    feat: Structured Streaming support

--- core/src/main/scala/dev/mauch/spark/dfio/DataFrameSource.scala
+++ core/src/main/scala/dev/mauch/spark/dfio/DataFrameSource.scala
@@ -2,8 +2,43 @@ package dev.mauch.spark.dfio
 
 import org.apache.spark.sql.DataFrame
 
-import java.time.Instant
+import java.net.URLDecoder
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{DataType, StructType}
 
 trait DataFrameSource {
   def read(): DataFrame
+}
+
+class StreamingDataFrameSource(
+  spark: SparkSession,
+  format: String,
+  path: String,
+  options: Map[String, String],
+  isStream: Boolean
+) extends DataFrameSource {
+  val schemaOpt: Option[StructType] = options.get("schema").map(s => DataType.fromJson(s).asInstanceOf[StructType])
+  override def read(): DataFrame = {
+    if (isStream) {
+      applyWatermark(
+        spark.readStream
+          .schema(schemaOpt.getOrElse(throw new IllegalArgumentException("schema is required for streaming")))
+          .format(format)
+          .options(options)
+          .load(path)
+      )
+    } else {
+      spark.read
+        .format(format)
+        .options(options)
+        .load(path)
+    }
+  }
+  def applyWatermark(df: DataFrame): DataFrame = {
+    val watermark = options.get("watermark").map(w => w.split(":").map(s => URLDecoder.decode(s, "UTF-8").trim))
+    watermark match {
+      case Some(Array(column, duration)) => df.withWatermark(column, duration)
+      case _ => df
+    }
+  }
 }
