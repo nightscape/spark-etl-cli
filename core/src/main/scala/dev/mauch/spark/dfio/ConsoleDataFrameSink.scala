c3b5b15feed09b6f9fb294be56eb2626d0c9d984
Martin Mauch <martin@mauch.dev>
2025-03-13 15:47:51 +0100

    feat: Structured Streaming support

--- core/src/main/scala/dev/mauch/spark/dfio/ConsoleDataFrameSink.scala
+++ core/src/main/scala/dev/mauch/spark/dfio/ConsoleDataFrameSink.scala
@@ -8,14 +8,18 @@ case class ConsoleDataFrameSink(spark: SparkSession) extends DataFrameSource wit
   override def read(): DataFrame =
     spark.emptyDataFrame
   override def write(df: DataFrame): Boolean = {
-    df.show(10000, false)
+    if (df.isStreaming) {
+      df.writeStream.outputMode("append").format("console").start()
+    } else {
+      df.show(10000, false)
+    }
     true
   }
 }
 
 class ConsoleUriParser extends DataFrameUriParser {
   def schemes: Seq[String] = Seq("console")
-  override def apply(uri: java.net.URI): SparkSession => DataFrameSource with DataFrameSink = {
-    spark => new ConsoleDataFrameSink(spark)
+  override def apply(uri: java.net.URI): SparkSession => DataFrameSource with DataFrameSink = { spark =>
+    new ConsoleDataFrameSink(spark)
   }
 }
