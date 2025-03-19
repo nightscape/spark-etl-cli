f9d85a72407fad895833f059163a445cf6e8ff29
Martin Mauch <martin@mauch.dev>
2025-03-19 16:35:16 +0100

    feat: values source can specify types in header

--- core/src/main/scala/dev/mauch/spark/dfio/ValuesSource.scala
+++ core/src/main/scala/dev/mauch/spark/dfio/ValuesSource.scala
@@ -6,50 +6,41 @@ import org.apache.spark.sql.types._
 import UriHelpers._
 import java.net.URI
 
-case class ValuesSource(spark: SparkSession, header: Seq[String], values: Seq[Seq[String]])
+case class ValuesSource(spark: SparkSession, header: Seq[StructField], values: Seq[Seq[String]])
     extends DataFrameSource
     with DataFrameSink {
   def valuesToDF(f: Seq[String] => Row): DataFrame = {
     import spark.implicits._
-    spark.createDataFrame(
-      java.util.Arrays.asList(values.map(f): _*),
-      StructType(header.map(StructField(_, StringType)))
-    )
+    spark.createDataFrame(java.util.Arrays.asList(values.map(f): _*), StructType(header))
   }
   override def read(): DataFrame = {
-    import spark.implicits._
-    header match {
-      case Seq(a) => valuesToDF { case Seq(a) => Row(a) }
-      case Seq(a, b) =>
-        valuesToDF { case Seq(a, b) => Row(a, b) }
-      case Seq(a, b, c) =>
-        valuesToDF { case Seq(a, b, c) => Row(a, b, c) }
-      case Seq(a, b, c, d) =>
-        valuesToDF { case Seq(a, b, c, d) => Row(a, b, c, d) }
-      case Seq(a, b, c, d, e) =>
-        valuesToDF { case Seq(a, b, c, d, e) => Row(a, b, c, d, e) }
-      case Seq(a, b, c, d, e, f) =>
-        valuesToDF { case Seq(a, b, c, d, e, f) => Row(a, b, c, d, e, f) }
+    def convertValue(s: String, dt: DataType): Any = dt match {
+      case IntegerType => s.toInt
+      case DoubleType => s.toDouble
+      case LongType => s.toLong
+      case _ => s
+    }
+    valuesToDF { row =>
+      Row.fromSeq(header.zip(row).map { case (field, value) => convertValue(value, field.dataType) })
     }
   }
   override def write(df: DataFrame): Boolean = {
     df.show(10000, false)
     true
   }
-
 }
 
 class ValuesUriParser extends DataFrameUriParser {
   def schemes: Seq[String] = Seq("values")
   override def apply(uri: java.net.URI): SparkSession => DataFrameSource with DataFrameSink = { spark =>
     val headerStr = uri.queryParams.getOrElse("header", "")
-    val parsedHeader: Seq[(String, DataType)] = headerStr
+    val parsedHeader: Seq[StructField] = headerStr
       .split(",")
       .filter(_.nonEmpty)
       .map { field =>
         val parts = field.split(":")
         if (parts.length == 2)
-          (
+          StructField(
             parts(0),
             parts(1).trim.toLowerCase match {
               case "int" => IntegerType
@@ -58,7 +49,7 @@ class ValuesUriParser extends DataFrameUriParser {
               case _ => StringType
             }
           )
-        else (field, StringType)
+        else StructField(field, StringType)
       }
       .toSeq
 
